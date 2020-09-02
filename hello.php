65c74498c41537d7393387cf07d23579a104a8f7
杨金桥 <1300786798@qq.com>
2020-09-02 10:51:02 +0800

    first commit

---
+++ hello.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+\Demo\Tools::Hello();
---
+++ src/Demo/Tools.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Demo;
+
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function Hello()
+    {
+        echo "hello world";
+    }
+}
